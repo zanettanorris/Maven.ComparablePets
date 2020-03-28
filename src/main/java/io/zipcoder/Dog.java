c4f91fd09ea54db8b1a7b1c0d3e6dcd7b092cf3a
April Howard <ahoward@zipcodes-MBP-5.fios-router.home>
2020-03-28 17:02:58 -0400

    tests created

---
+++ src/main/java/io/zipcoder/Dog.java
@@ -0,0 +1,21 @@
+package io.zipcoder;
+
+public class Dog implements Pet {
+
+    public int getNumberOfDogs;
+
+    public Dog() {
+
+    }
+
+    public void setName() {
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public String speak() {
+        return null;
+    }
+}
---
+++ src/test/java/io/zipcoder/PetTest.java
@@ -0,0 +1,33 @@
+package io.zipcoder;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PetTest {
+    @Test
+    public void testInheritanceDog() {
+        Dog dog = new Dog();
+        Assert.assertTrue(dog instanceof Pet);
+    }
+
+    @Test
+    public void testInheritanceCat() {
+        Cat cat = new Cat();
+        Assert.assertTrue(cat instanceof Pet);
+    }
+
+    @Test
+    public void testAnimalInheritance() {
+        //Given
+        Boolean expected = true;
+
+        //When
+        Zebra zebra = new Zebra();
+
+        //Then
+        Boolean animalInheritance = true;
+        Assert.assertTrue(animalInheritance);
+
+    }
+}
+
